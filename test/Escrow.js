ba482516604b592ac44ae908da6886ddcb8670b5
yashmandi <yashmandi18@gmail.com>
2024-09-04 17:59:28 +0530

    describe

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,59 +1,54 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
+describe("Escrow", () => {
+  let buyer, seller;
+  let realEstate, escrow;
 
-    let buyer, seller
-    let realEstate, escrow
+  describe("Deployment", async () => {
+    it("Returns NFT address", async () => {});
 
-    it('Returns NFT address', async() => {
+    it("Returns seller", async () => {});
 
-    })
+    it("Returns inspector", async () => {});
 
-    it('Returns seller', async() => {
-        
-    })
+    it("Returns render", async () => {});
+  });
 
-    it('Returns inspector', async() => {
-        
-    })
+  it("saves the address", async () => {
+    // setup accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
 
-    it('Returns render', async() => {
-        
-    })
+    // deploy real estate
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+    await realEstate.deployed();
+    console.log(realEstate.address);
 
-    it('saves the address', async() => {
+    // mint
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodywmPekYgbnXGo4DFubJiLc2EB/1.json"
+      );
+    await transaction.wait();
 
-        // setup accounts
-        [buyer, seller, inspector, lender] = await ethers.getSigners()
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
 
-        // deploy real estate
-        const RealEstate = await ethers.getContractFactory('RealEstate');
-        realEstate = await RealEstate.deploy();
-        await realEstate.deployed();
-        console.log(realEstate.address);
+    const result = await escrow.nftAddress();
+    expect(result).to.be.equal(realEstate.address);
 
-        // mint
-        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodywmPekYgbnXGo4DFubJiLc2EB/1.json");
-        await transaction.wait();
-
-        const Escrow = await ethers.getContractFactory('Escrow');
-        escrow = await Escrow.deploy(
-            realEstate.address,
-            seller.address,
-            inspector.address,
-            lender.address
-        )
-
-        const result = await escrow.nftAddress()
-        expect(result).to.be.equal(realEstate.address)
-
-        result = await escrow.seller()
-        expect(result).to.be.equal(seller.address) 
-
-    })
-})
+    result = await escrow.seller();
+    expect(result).to.be.equal(seller.address);
+  });
+});
